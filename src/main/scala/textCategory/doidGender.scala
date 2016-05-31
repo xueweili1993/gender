39cda1444b975ac2c2b6e77366d0fc2880d31321
sx0404 <s1264242822@163.com>
2016-05-31 18:16:16 +0800

    5-31-14

---
+++ src/main/scala/textCategory/doidGender.scala
@@ -0,0 +1,73 @@
+package textCategory
+
+import org.apache.spark.{SparkConf, SparkContext}
+import java.sql.DriverManager
+
+import scala.collection.mutable.Map
+import org.apache.spark.sql.SQLContext
+
+/**
+  * Created by xinmei on 16/5/31.
+  */
+object doidGender {
+
+
+  def main (args: Array[String])={
+
+    val conf = new SparkConf()
+
+    val sc = new SparkContext(conf)
+
+
+    val hdfspath1 = "hdfs:///gaoy/duid2AppsWithLabel/part-00000"
+    val hdfspath2 = "hdfs:////gaoy/genderLabeledData/part-00000"
+
+    //val savepath1 = "hdfs:///lxw/"
+
+
+    val hadoopConf = sc.hadoopConfiguration
+
+
+    val awsAccessKeyId = args(0)
+    val awsSecretAccessKey = args(1)
+
+    val anchordate = "20160426"
+
+    hadoopConf.set("fs.s3n.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
+
+    hadoopConf.set("fs.s3n.awsAccessKeyId", awsAccessKeyId)
+
+    hadoopConf.set("fs.s3n.awsSecretAccessKey", awsSecretAccessKey)
+
+
+    val idWithapp = sc.textFile(hdfspath1)
+      .map{case line =>
+
+        val first = line.replaceAll("\\(|\\)","")
+
+        val lineArray = first.split(",",2)
+
+        lineArray(0)
+
+
+      }.distinct
+
+    val idWithgender = sc.textFile(hdfspath2)
+      .map{case line=>
+
+          val linearray = line.split("/t")
+          linearray(0)
+      }.distinct
+    val joined = idWithgender.intersection(idWithapp)
+
+    val appnum= idWithapp.count()
+    val gendernum = idWithgender.count()
+    val joinnum = joined.count()
+
+    println ("lxw " + appnum)
+    println ("lxw " + gendernum)
+    println ("lxw " + joinnum)
+
+  }
+
+}
